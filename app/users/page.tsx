9634985d9b3a77e78918b4563da052c248291c13
Abe Flores <a.flrs182@gmail.com>
2024-01-17 17:54:21 -0800

    adjusted the page.tsx file inside of users to fetch dummy user data and display the id and name on the /users page, check the userspage function in page.tsx

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const UsersPage = () => {
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: User[] = await res.json()
+
   return (
-    <div className='text-black'>UsersPage</div>
+    <>
+    <h1>users</h1>
+    <ul className='text-black'>
+      {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
+    </>
   )
 }
 
