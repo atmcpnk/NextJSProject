9f895ac38ed215421f86f9c170ca345107bd7cae
Abe Flores <a.flrs182@gmail.com>
2024-01-17 18:24:12 -0800

    disabled caching and updated /users to server-side rendering tested using the date,tolocaletimestring functionality to test refresh.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,13 +7,13 @@ interface User {
 
 const UsersPage = async () => {
   const res = await fetch(
-    'https://jsonplaceholder.typicode.com/users',
-    { next: { revalidate: 10} });
+    'https://jsonplaceholder.typicode.com/users',{ cache: 'no-store'});
   const users: User[] = await res.json()
 
   return (
     <>
-    <h1>users</h1>
+    <h1 className='text-black'>Users</h1>
+    <p className='text-black'>{new Date().toLocaleTimeString()}</p>
     <ul className='text-black'>
       {users.map(user => <li key={user.id}>{user.name}</li>)}
     </ul>
