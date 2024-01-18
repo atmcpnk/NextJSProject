4f90b3e30458b4055513b9c82a4b964343154782
Abe Flores <a.flrs182@gmail.com>
2024-01-17 16:57:14 -0800

    created a new folder inside of users, with a new page.tsx file, understanding new pages lead to new url slugs for the site. replaced anchor tags with link tags in tsx to optimize loading content on pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main><h1 className='text-black'>Hello World</h1></main>
+    <main>
+      <h1 className='text-black'>Hello World</h1>
+      <Link href='/users' className='text-black'>Users</Link>
+    </main>
   )
 }
