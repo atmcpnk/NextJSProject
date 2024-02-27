fde281361089213ee337297cc0ab34035247080c
Abe Flores <a.flrs182@gmail.com>
2024-02-26 16:45:48 -0800

    added a PUT function to the route file inside of api>users>[id] that validates and updates a user with the given id, and returns the updated user, error parameters are set for 400 and 404, incase there is a requirement or no user is found.

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,3 +10,20 @@ export function GET(
             return NextResponse.json({ error: 'user not found'}, { status: 404})
         return NextResponse.json({ id: 1, name: 'john'});
 }
+
+export async function PUT(
+    request: NextRequest, 
+    { params }: { params: { id: number} }){
+    // Validate the request body
+    const body = await request.json();
+    if (!body.name)
+    // if invalid, return 400 error
+        return NextResponse.json({error: "Name is required"}, { status: 400});
+    // else, Fetch the user with the given id from the db
+    if (params.id > 10)
+    // if doesn't exist, return 404 error
+        return NextResponse.json({ error: 'User not found'}, { status: 404});
+    // update the user with the given id
+    // Return the updated user
+    return NextResponse.json({ id: 1, name: body.name});
+}
