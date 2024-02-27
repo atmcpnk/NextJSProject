27f8709c702b16fe4649686f06538bf89de3e476
Abe Flores <a.flrs182@gmail.com>
2024-02-26 16:52:44 -0800

    added a DELETE function in api>users>[id]>routes file that deletes the user when a request is made and returns an empty object.

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -27,3 +27,14 @@ export async function PUT(
     // Return the updated user
     return NextResponse.json({ id: 1, name: body.name});
 }
+
+export function DELETE(request: NextRequest, 
+    { params }: { params: { id: number} }){
+        // Fetch user from db 
+        // if not found, return 404 error
+        if (params.id > 10)
+            return NextResponse.json({error: 'User not found'}, { status: 404});
+        return NextResponse.json({});
+        // delete the user
+        // return 200 status
+    }
