4f69012ba4fe316b2d59c50807cbdf8d9ab4cb65
Abe Flores <a.flrs182@gmail.com>
2024-02-26 16:34:20 -0800

    Added an asynchronus POST function that validates and creates a new object based on the input name property, if no name, then it is invalid and returns a 400 error.

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -12,3 +12,13 @@ export function GET(request: NextRequest){
         {id: 4, name: 'smith'},
     ]);
 }
+
+export async function POST( request: NextRequest ) {
+   const body = await request.json();
+   // Validate
+   // if invalid, return 400 error
+   // else, return the data
+   if(!body.name)
+    return NextResponse.json({ error: 'name is required'}, { status: 400});
+   return NextResponse.json({ id: 1, name: body.name }, { status: 201});
+}
